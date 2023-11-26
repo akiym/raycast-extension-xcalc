6235bd40de0b96d79341ab41f3b7b0362010e655
Takumi Akiyama <t.akiym@gmail.com>
2023-11-26 21:28:48 +0900

    add raycast-env.d.ts

---
+++ raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `xcalc` command */
+  export type Xcalc = ExtensionPreferences & {}
+  /** Preferences accessible in the `xconvert` command */
+  export type Xconvert = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `xcalc` command */
+  export type Xcalc = {}
+  /** Arguments passed to the `xconvert` command */
+  export type Xconvert = {}
+}
+
+
