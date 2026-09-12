5d87a342b69fea1f17911ef0f4608ed5c326fe5a
Takumi Akiyama <t.akiym@gmail.com>
2026-09-12 18:08:25 +0900

    import from latest pwntools

--- assets/safeeval.py
+++ assets/safeeval.py
@@ -35,14 +35,17 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from types import CodeType
+from typing import Any
 
 _const_codes = [
     'POP_TOP','ROT_TWO','ROT_THREE','ROT_FOUR','DUP_TOP',
-    'BUILD_LIST','BUILD_MAP','BUILD_TUPLE','BUILD_SET',
+    'BUILD_LIST','BUILD_MAP', 'MAP_ADD', 'BUILD_TUPLE','BUILD_SET',
     'BUILD_CONST_KEY_MAP', 'BUILD_STRING',
-    'LOAD_CONST','RETURN_VALUE','STORE_SUBSCR', 'STORE_MAP',
-    'LIST_TO_TUPLE', 'LIST_EXTEND', 'SET_UPDATE', 'DICT_UPDATE', 'DICT_MERGE',
-    ]
+    'LOAD_CONST','LOAD_SMALL_INT','RETURN_VALUE','STORE_SUBSCR', 'STORE_MAP',
+    'LIST_TO_TUPLE', 'LIST_APPEND', 'LIST_EXTEND', 'SET_ADD', 'SET_UPDATE', 'DICT_UPDATE', 'DICT_MERGE',
+    'COPY', 'RESUME', 'RETURN_CONST'
+]
 
 _expr_codes = _const_codes + [
     'UNARY_POSITIVE','UNARY_NEGATIVE','UNARY_NOT',
@@ -51,27 +54,30 @@ _expr_codes = _const_codes + [
     'BINARY_MODULO','BINARY_ADD','BINARY_SUBTRACT',
     'BINARY_LSHIFT','BINARY_RSHIFT','BINARY_AND','BINARY_XOR',
     'BINARY_OR',
-    ]
+    'BINARY_OP',
+]
 
+_values_codes = _expr_codes + ['LOAD_NAME']
 
-def _get_opcodes(codeobj):
+def _get_opcodes(codeobj: CodeType) -> list[int]:
+    """_get_opcodes(codeobj) -> [opcodes]
+
+    Extract the actual opcodes as a list from a code object
+
+    >>> c = compile("[1 + 2, (1,2)]", "", "eval")
+    >>> _get_opcodes(c)  # doctest: +SKIP
+    [...100, 100, 103, 83]
+    """
     import dis
-    if hasattr(dis, 'get_instructions'):
-        return [ins.opcode for ins in dis.get_instructions(codeobj)]
-    i = 0
-    opcodes = []
-    s = codeobj.co_code
-    while i < len(s):
-        code = s[i]
-        opcodes.append(code)
-        if code >= dis.HAVE_ARGUMENT:
-            i += 3
-        else:
-            i += 1
-    return opcodes
+    return [ins.opcode for ins in dis.get_instructions(codeobj)]
 
+def test_expr(expr: str, allowed_codes: list[str]) -> CodeType:
+    """test_expr(expr, allowed_codes) -> codeobj
 
-def test_expr(expr, allowed_codes):
+    Test that the expression contains only the listed opcodes.
+    If the expression is valid and contains only allowed codes,
+    return the compiled code object. Otherwise raise a ValueError
+    """
     import dis
     allowed_codes = [dis.opmap[c] for c in allowed_codes if c in dis.opmap]
     try:
@@ -84,11 +90,93 @@ def test_expr(expr, allowed_codes):
             raise ValueError("opcode %s not allowed" % dis.opname[code])
     return c
 
+def const(expr: str) -> Any:
+    """const(expression) -> value
 
-def expr(expr):
+    Safe Python constant evaluation
+
+    Evaluates a string that contains an expression describing
+    a Python constant. Strings that are not valid Python expressions
+    or that contain other code besides the constant raise ValueError.
+
+    Examples:
+
+        >>> const("10")
+        10
+        >>> const("[1,2, (3,4), {'foo':'bar'}]")
+        [1, 2, (3, 4), {'foo': 'bar'}]
+        >>> const("[1]+[2]")
+        Traceback (most recent call last):
+        ...
+        ValueError: opcode BINARY_ADD not allowed
+        >>> const("[" + ",".join(str(i) for i in range(40)) + "]") == list(range(40))
+        True
+        >>> const("{" + ",".join(str(i) for i in range(40)) + "}") == set(range(40))
+        True
+    """
+
+    c = test_expr(expr, _const_codes)
+    return eval(c)
+
+def expr(expr: str) -> Any:
+    """expr(expression) -> value
+
+    Safe Python expression evaluation
+
+    Evaluates a string that contains an expression that only
+    uses Python constants. This can be used to e.g. evaluate
+    a numerical expression from an untrusted source.
+
+    Examples:
+
+        >>> expr("1+2")
+        3
+        >>> expr("[1,2]*2")
+        [1, 2, 1, 2]
+        >>> expr("__import__('sys').modules")
+        Traceback (most recent call last):
+        ...
+        ValueError: opcode LOAD_NAME not allowed
+    """
+
     c = test_expr(expr, _expr_codes)
     return eval(c)
 
+def values(expr: str, env: dict[str, Any]) -> Any:
+    """values(expression, dict) -> value
+
+    Safe Python expression evaluation
+
+    Evaluates a string that contains an expression that only
+    uses Python constants and values from a supplied dictionary.
+    This can be used to e.g. evaluate e.g. an argument to a syscall.
+
+    Note: This is potentially unsafe if e.g. the __add__ method has side
+          effects.
+
+    Examples:
+
+        >>> values("A + 4", {'A': 6})
+        10
+        >>> class Foo:
+        ...    def __add__(self, other):
+        ...        print("Firing the missiles")
+        >>> values("A + 1", {'A': Foo()})
+        Firing the missiles
+        >>> values("A.x", {'A': Foo()})
+        Traceback (most recent call last):
+        ...
+        ValueError: opcode LOAD_ATTR not allowed
+    """
+
+    # The caller might need his dictionary again
+    env = dict(env)
+
+    # We do not want to have built-ins set
+    env['__builtins__'] = {}
+
+    c = test_expr(expr, _values_codes)
+    return eval(c, env)
 
 if __name__ == '__main__':
     import sys
